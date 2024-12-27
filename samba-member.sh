8bc7ea0017a6bdc1a7f9e0999bf53bb16e093fb7
Mikael Correa <142676918+mikaelrc7@users.noreply.github.com>
2024-12-27 10:46:06 -0300

    Update samba-member.sh

--- samba-member.sh
+++ samba-member.sh
@@ -1,19 +1,10 @@
-=Script para ingresso do linux no domínio (Ubuntu 24.04):=
-
-* Permite logon com usuários do domínio, incluindo cache para logon offline.
-
-* Testado no Linux Mint 22 Cinnamon
-
-* Ajustar variáveis para o domínio escolhido
-
-<pre>	
 #!/bin/bash
 
-REALM=TRUSTDC.LOCAL
-DOMAIN=trustdc.local
-WORKGROUP=TRUSTDC
-SERVER=samba-mikael.trustdc.local
-SAMBA_IP=192.168.16.41
+REALM=DOMAIN.LOCAL
+DOMAIN=domain.local
+WORKGROUP=DOMAIN
+SERVER=server.domain.local
+SAMBA_IP=192.168.x.x
 ADMIN_USER=administrator
 
 
@@ -26,16 +17,15 @@ echo $ADMIN_USER
 
 apt update
 
-#apt upgrade -y
 
 DEBIAN_FRONTEND=noninteractive apt install -y samba winbind krb5-user libpam-krb5 libnss-winbind libpam-winbind vim openssh-server
 
 
-# Adicionar linha para criar o diretório home automaticamente no login do usuário samba:
+# Enable user home directory creation:
 echo "session required pam_mkhomedir.so skel=/etc/skel/ umask=0077" >> /etc/pam.d/common-session
 
 
-# Configuracao KRB5.CONF
+# Config KRB5.CONF
 echo "
 [libdefaults]
 	default_realm = $REALM
@@ -62,7 +52,7 @@ echo "
 	$DOMAIN = $REALM" > /etc/krb5.conf
 
 
-# Configuracao PAM_WINBIND.CONF
+# Config PAM_WINBIND.CONF
 
 echo "
 # pam_winbind configuration file
@@ -75,7 +65,7 @@ echo "
 cached_login = yes" > /etc/security/pam_winbind.conf
 	
 	
-# Configuracao SMB.CONF	
+# Config SMB.CONF	
 echo "
 	   [global]
 	   workgroup = $WORKGROUP
@@ -93,7 +83,7 @@ echo "
 	   winbind enum groups = yes
 	   winbind use default domain = yes
 	   winbind refresh tickets = yes
-	   # As duas linhas seguintes são necessárias para habilitar o login offline com usuários do dominio
+	   
 	   winbind offline logon = yes
 	   winbind request timeout = 10
 
@@ -103,7 +93,7 @@ echo "
 	   template homedir = /home/%D/%U" > /etc/samba/smb.conf
 	   
 
-# Configuracao NSSWITCH.CONF
+# Config NSSWITCH.CONF
 echo "
 passwd:         files systemd winbind
 group:          files systemd winbind
@@ -135,35 +125,14 @@ net ads join -U $ADMIN_USER
 systemctl restart smb winbind
 
 
-# Ajustar troca de usuário no cinnamon:
-#sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash nomodeset"/g' /etc/default/grub
-
+# Enable manual user switching on Mint Cinnamon:
 echo "
 [Seat:*]
 greeter-show-manual-login=true" > /etc/lightdm/lightdm.conf
 
 
 
-####################################################################
-
-### Executar os seguintes comandos para executar o script:
-	
-### Baixar esse arquivo
-
-### chmod +x ~/Downloads/samba-member.sh
- 
-### sudo sh ~/Downloads/samba-member.sh
-
-### Inserir senha do usuário administrador quando solicitada
-
-### Ao finalizar a execução, reiniciar a máquina
-
-####################################################################
-
-### Fontes:
+### Source:
 # https://wiki.samba.org/index.php/PAM_Offline_Authentication
 # https://wiki.samba.org/index.php/Setting_up_Samba_as_a_Domain_Member
 
-
-
-</pre>
